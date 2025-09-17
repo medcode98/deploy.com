ca551c960588fc0d3593ae84720cb3e266c7db70
medcode98 <medcode98@gmail.com>
2025-09-17 10:47:05 +0100

    move man

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+     <h1 className="bg-red-500 text-white text-center">heading</h1>
+     <p>This is a paragraph</p>
+    </>
+  )
+}
+
+export default App
